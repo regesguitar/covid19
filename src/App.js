a2014ec595f7dc7d8cc9fee7b6855cf4ad0e1aaf
Bruno Reges <bruno.reges@hp.com>
2020-11-24 21:09:12 -0300

    criando projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
+import React, {useState} from 'react'
+import { MenuItem, FormControl, Select, } from "@material-ui/core"
 import './App.css';
 
+
 function App() {
+ const [countries, setCountries] = useState ([
+   'USA', 'UK', 'Brazil'
+ ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_header">
+        <h1>Aplicação Covid-19 Estrutura de Dados</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+
+            {/* all the coountries - list of the options */}
+
+            {countries.map((country)=>(
+                <MenuItem value={country}>{country}</MenuItem>
+              ) )
+            }
+
+            {/* <MenuItem value="worlwide">Worldwide</MenuItem>
+            <MenuItem value="worlwide">Option two</MenuItem>
+            <MenuItem value="worlwide">Option three</MenuItem>
+            <MenuItem value="worlwide">Yo</MenuItem> */}
+          </Select>
+
+        </FormControl>
+
+      </div>
+
+
+
+
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* MAP */}
     </div>
   );
 }
